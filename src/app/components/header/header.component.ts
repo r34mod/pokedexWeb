765ffe50ccbc63d92e4efcf707fc81c6b0e76ce0
r34mod <48959634+r34mod@users.noreply.github.com>
2022-01-10 12:12:57 +0100

    Add files via upload

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router, _route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+  }
+
+  home(){
+    this.router.navigateByUrl('/home');
+  }
+
+}
